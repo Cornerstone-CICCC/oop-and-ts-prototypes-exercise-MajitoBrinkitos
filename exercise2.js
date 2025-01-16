e1c60358a31c0cd855f2f0a3bf0db487571ed833
Majito Brinkitos <mcvef2013@gmail.com>
2025-01-16 11:12:42 -0800

    exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -4,11 +4,34 @@
 // 2. Create another constructor function called "Smartphone" that inherits from Device, and add a "model" property to Smartphone.
 // 3. Add a method "call" to Smartphone's prototype that logs "Calling from [brand] [model]..."
 
+const { prototype } = require("events")
+
 
 
 // YOUR CODE HERE
+//function Device
+function Device (brand) {
+    this._brand = brand
+}
 
+//method powerOn
+Device.prototype.powerOn = function() {
+    console.log(`The ${this._brand} device is now powered on.`)
+}
 
+//function Smartphone
+function Smartphone(brand, model) {
+    Device.call(this, brand)
+    this._model = model
+}
+
+Smartphone.prototype = Object.create(Device.prototype) //Device properties into Smartphone
+Smartphone.prototype.constructor = Smartphone
+
+//method call
+Smartphone.prototype.call = function() {
+    console.log(`Calling from ${this._brand} ${this._model}`)
+}
 
 
 // Test the Smartphone prototype with inheritance
